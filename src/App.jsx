6d8c8597b2a62e4e2e2ed375504f7f23ec9105f9
xGODRICx <chris.a.meyer@outlook.com>
2023-06-01 09:43:57 -0500

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
 
+  console.log(puppies)
   return (
-    <>
+    
       <div>
-        
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
     
-    </>
-  )
+  );
 }
 
 export default App
 
-console.log(puppyList)
